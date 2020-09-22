d187ed7a624ee2a364300eabeeb2c84a6da1dd6f
Trọng Hiếu <tronghieu60s@gmail.com>
2020-09-22 20:38:53 +0700

    render logic pagination

--- components/UI/Pagination/index.jsx
+++ components/UI/Pagination/index.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import getPagination from 'helpers/pagination';
 
 const PaginationUI = (props) => {
-  const { pagination } = props;
+  const { currentItem, totalItems, pageSize } = props;
+  let currentPage = Math.floor(currentItem / pageSize);
+  currentPage = currentItem % pageSize === 0 ? currentPage : currentPage + 1;
+
+  const pagination = getPagination(totalItems, currentPage, pageSize);
+  const { startPage, endPage, startIndex, endIndex } = pagination;
+
+  const renderPaginationItem = (start, end, current, total) => {
+    const result = [];
+    for (let i = start + 1; i <= end; i += 1)
+      if (i <= total)
+        result.push(
+          <Pagination.Item key={i} active={current === i}>
+            {i}
+          </Pagination.Item>,
+        );
+    return result;
+  };
 
   return (
     <Pagination>
-      <Pagination.First />
-      <Pagination.Prev />
-
-      <Pagination.Item>{11}</Pagination.Item>
-      <Pagination.Item active>{12}</Pagination.Item>
-      <Pagination.Item>{13}</Pagination.Item>
-
-      <Pagination.Next />
-      <Pagination.Last />
+      {currentPage !== startPage && <Pagination.First />}
+      {currentItem > 1 && <Pagination.Prev />}
+      {renderPaginationItem(startIndex, endIndex, currentItem, totalItems)}
+      {currentItem < totalItems && <Pagination.Next />}
+      {currentPage !== endPage && <Pagination.Last />}
     </Pagination>
   );
 };
 
 PaginationUI.propTypes = {
-  pagination: PropTypes.shape({
-    totalItems: PropTypes.number,
-    currentPage: PropTypes.number,
-    pageSize: PropTypes.number,
-    totalPages: PropTypes.number,
-    startPage: PropTypes.number,
-    endPage: PropTypes.number,
-    startIndex: PropTypes.number,
-    endIndex: PropTypes.number,
-  }),
+  currentItem: PropTypes.number,
+  totalItems: PropTypes.number,
+  pageSize: PropTypes.number,
 };
 
 PaginationUI.defaultProps = {
-  pagination: {
-    totalItems: 0,
-    currentPage: 0,
-    pageSize: 0,
-    totalPages: 0,
-    startPage: 0,
-    endPage: 0,
-    startIndex: 0,
-    endIndex: 0,
-  },
+  currentItem: 10,
+  totalItems: 10,
+  pageSize: 4,
 };
 
 export default PaginationUI;
